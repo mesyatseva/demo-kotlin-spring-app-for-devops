6aba23451596c9c474f5123497d21e052e5f2433
nmescv <mescvnv@gmail.com>
2023-04-15 15:43:29 +0300

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "demo-kotlin-spring-app-for-devops"
---
+++ src/main/kotlin/com/github/nmescv/demokotlinspringappfordevops/DemoKotlinSpringAppForDevopsApplication.kt
@@ -0,0 +1,11 @@
+package com.github.nmescv.demokotlinspringappfordevops
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoKotlinSpringAppForDevopsApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoKotlinSpringAppForDevopsApplication>(*args)
+}
